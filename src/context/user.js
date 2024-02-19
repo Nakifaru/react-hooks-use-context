39add2fd36dbea6f9ef0c3b09e08ef7189280bab
Michael <michaelmwendwamumo@gmail.com>
2024-02-19 13:50:09 +0300

    Completed the code along

---
+++ src/context/user.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const UserContext = React.createContext();
+
+function UserProvider ({children}) {
+    const currentUser = {
+        name: 'Duane',
+        interests: ['Coding', 'Biking', "Words ending in 'ing' ",]
+    }
+    return (
+        <UserContext.Provider value={currentUser}>
+            {children}
+        </UserContext.Provider>
+    )
+}
+
+export { UserContext, UserProvider }
